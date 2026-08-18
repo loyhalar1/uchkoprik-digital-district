9f648c274c4d4eb3e532e7607a99ba9d60f192e1
loyhalar1 <mvaziyatlar@gmail.com>
2026-08-18 17:56:44 +0500

    Update supabase.js

--- public/assets/js/supabase.js
+++ public/assets/js/supabase.js
@@ -1,10 +1,3 @@
-const SUPABASE_URL =
-  "https://uutibmowhszwtfcxkwrr.supabase.co";
-
-const SUPABASE_KEY =
-  "sb_publishable_vsI_Mg4X96Ap3U0IflYhQw_tLIxXRRU";
-
-window.sb = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const SUPABASE_URL = "https://uutibmowhszwtfcxkwrr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vsI_Mg4X96Ap3U0IflYhQw_tLIxXRRU";
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
