6150743c076f70eab2ba8093328c9f3a7072f424
loyhalar1 <mvaziyatlar@gmail.com>
2026-08-18 12:59:49 +0500

    Create supabase.js

---
+++ public/assets/js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://uutibmowhszwtfcxkwrr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_vsI_Mg4X96Ap3U0IflYhQw_tLIxXRRU";
+
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
